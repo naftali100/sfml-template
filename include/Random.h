25c1ad714711e802395574ceed0c6ea8af626ee7
naftali <n.besyakov@gmail.com>
2022-05-27 12:55:20 +0300

    fix random

--- include/Random.h
+++ include/Random.h
@@ -5,17 +5,17 @@
 #include <random>
 
 namespace Random {
-    inline std::random_device device_;
-    inline std::default_random_engine engine_(device_());
+std::random_device device_;
+std::default_random_engine engine_(device_());
 
-    inline int rnd(int a, int b) {
-        std::uniform_int_distribution<int> uni_dist(a, b);
-        return uni_dist(engine_);
-    };
-    inline double rnd(double a, double b) {
-        std::uniform_real_distribution<double> uni_dist(a, b);
-        return uni_dist(engine_);
-    };
+inline int rnd(int a, int b) {
+    std::uniform_int_distribution<int> uni_dist(a, b);
+    return uni_dist(engine_);
 };
+inline double rnd(double a, double b) {
+    std::uniform_real_distribution<double> uni_dist(a, b);
+    return uni_dist(engine_);
+};
+};  // namespace Random
 
 #endif
