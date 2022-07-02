e36517079cdf07094f14b5742944fae84a7e27fc
naftali <n.besyakov@gmail.com>
2022-07-02 21:56:54 +0300

    update pch - add more std includes

--- include/pch.h
+++ include/pch.h
@@ -2,16 +2,26 @@
 
 #include <imgui-SFML.h>
 #include <imgui.h>
+
+#include <plog/Helpers/PrintVar.h>
 #include <plog/Log.h>
 
-#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
 #include <iostream>
 #include <map>
+#include <memory>
+#include <ranges>
 #include <set>
 #include <stack>
+#include <utility>
 #include <vector>
-#include <array>
-#include <functional>
-#include <memory>
+
+namespace rng = std::ranges;
+// basic lambda with no params that return void
+using Func = std::function<void()>;
