1acc65bc89e4d1894325e85182df1f5b0e44b8f7
naftali <n.besyakov@gmail.com>
2022-07-02 22:09:02 +0300

    update stateManager test

--- tests/StateManager_test.cpp
+++ tests/StateManager_test.cpp
@@ -1,23 +1,26 @@
-#include "doctest/doctest.h"
-#include <SFML/Graphics.hpp>
 #include "StateManager.h"
-// #include "States/MainMenu.h"
-#include "States/DemoState.h"
 
-TEST_CASE("test state manager"){
-    sf::RenderWindow win(sf::VideoMode(10, 10), "world", sf::Style::Close);
+#include <SFML/Graphics.hpp>
+#include <exception>
 
+#include "ExampleStates/DemoState.h"
+#include "doctest/doctest.h"
+
+sf::RenderWindow win;
+
+TEST_CASE("test state manager") {
     StateManager sm(win);
 
     sm.pushState(std::make_unique<DemoState>(sm));
+
     CHECK(sm.isRunning() == true);
 
-    SUBCASE("pop state"){
+    SUBCASE("pop state") {
         sm.popState();
         CHECK(sm.isRunning() == false);
     }
 
-    SUBCASE("stop"){
+    SUBCASE("stop") {
         sm.stop();
         CHECK(sm.isRunning() == false);
     }
