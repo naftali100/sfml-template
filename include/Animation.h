0cceedf62babb7bcc0d4c8dd05a0023a5cf09af2
naftali <n.besyakov@gmail.com>
2022-07-02 21:41:09 +0300

    update animation - easy init & sprite storage

--- include/Animation.h
+++ include/Animation.h
@@ -2,6 +2,8 @@
 #ifndef ANIMATION_H
 #define ANIMATION_H
 
+#include "Resources.h"
+
 struct Frame {
     sf::IntRect rect;
     double duration;  // in seconds
@@ -9,19 +11,30 @@ struct Frame {
 
 class Animation {
 public:
-    Animation(sf::Sprite &target);
-    // virtual ~Animation();
-    void addFrame(Frame &&frame);
-    void update(double elapsed);
-    const double getLength() const {
+    explicit Animation(sf::Sprite& target);
+    Animation(const Textures::ID& t, const sf::Vector2f& pos);
+    void initFramesWithFixedSize(const sf::Vector2u& textureSize, int atlasRows, int atlasCols, float frameTime);
+    void addFrame(Frame&& frame);
+    // select frame to show manually
+    void setFrame(int index);
+    // Set the length of time the animation will play for
+    void setDuration(float time);
+    void update(const sf::Time& elapsed);
+    double getLength() const {
         return totalLength;
+    }
+    sf::Sprite& getTarget() {
+        return m_target;
     }
 
 private:
-    std::vector<Frame> frames;
+    std::vector<Frame> m_frames;
     double totalLength;
     double progress;
-    sf::Sprite &target;
+    sf::Sprite& m_target;
+    sf::Sprite m_spriteStorage;
+    sf::Vector2f m_pos;  // used only when using sprit storage
+    float m_duration = -1;
 };
 
 #endif
--- src/Impl/Animation.cpp
+++ src/Impl/Animation.cpp
@@ -1,28 +1,59 @@
 #include "Animation.h"
 
-Animation::Animation(sf::Sprite& target) : target(target) {
+Animation::Animation(sf::Sprite& target) : m_target(target) {
     progress = totalLength = 0.0;
+}
+
+Animation::Animation(const Textures::ID& t, const sf::Vector2f& pos) : m_target(m_spriteStorage) {
+    m_spriteStorage.setTexture(TextureHolder::get(t));
+    m_spriteStorage.setPosition(pos);
 }
 
 void Animation::addFrame(Frame&& frame) {
     totalLength += frame.duration;
-    frames.push_back(std::move(frame));
+    m_frames.push_back(std::move(frame));
 }
 
-void Animation::update(double elapsed) {
-    progress += elapsed;
-    double p = progress;
-    for (size_t i = 0; i < frames.size(); i++) {
-        p -= frames[i].duration;
+void Animation::setDuration(float d) {
+    m_duration = d;
+}
+
+void Animation::update(const sf::Time& elapsed) {
+    LOGV << "start";
+    if (m_duration > 0 && progress >= m_duration) {
+        m_target.setTextureRect({0, 0, 0, 0});
+        return;
+    }
+    progress += elapsed.asSeconds();
+    auto p = std::fmod(progress, totalLength);
+    for (size_t i = 0; i < m_frames.size(); i++) {
+        p -= m_frames.at(i).duration;
 
         // if we have progressed OR if we're on the last frame, apply and stop.
-        if (p > 0.0 && &(frames[i]) == &frames.back()) {
+        if (p > 0.0 && &(m_frames.at(i)) == &m_frames.back()) {
             i = 0;     // start over from the beginning
             continue;  // break off the loop and start where i is
         }
         if (p <= 0.0) {
-            target.setTextureRect(frames[i].rect);
+            m_target.setTextureRect(m_frames.at(i).rect);
             break;  // we found our frame
         }
     }
+    LOGV << "finish";
+}
+
+void Animation::initFramesWithFixedSize(const sf::Vector2u& textureSize, int atlasRows, int atlasCols,
+                                        float frameTime) {
+    int FrameXSize = textureSize.x / atlasCols;
+    int FrameYSize = textureSize.y / atlasRows;
+
+    for (int i = 0; i < atlasRows; i++) {
+        for (int j = 0; j < atlasCols; j++) {
+            addFrame({sf::IntRect(j * FrameXSize, i * FrameYSize, FrameXSize, FrameYSize), frameTime});
+        }
+    }
+}
+
+void Animation::setFrame(int index) {
+    m_target.setTextureRect(m_frames.at(index).rect);
 }
